452ebe0e45f4a73edc10e528fc265beef0be7314
Nazaruk-D <nazaruk-dima@mail.ru>
2022-07-28 14:03:58 +0300

    added styles for active buttons in the menu

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,19 @@ import {MainCounter} from "./CounterV1/MainCounter";
 import {MainCounterV2} from "./CounterV2/MainCounterV2";
 import {NavLink, Route, Routes} from "react-router-dom";
 
+
 function App() {
 
     return (<>
             <div className={"counterMenu"}>Сounter menu</div>
         <div className={"nav"}>
-           <NavLink to={'/'} className={"nav1v"}>First counter</NavLink>
-           <NavLink to={'/option2'} className={"nav2v"}>Second counter</NavLink>
+           <NavLink to={'/Counter'} className={"nav1v"} style={(params)=>{return {color: params.isActive ? 'gold' : "white"}}}>First counter</NavLink>
+           <NavLink to={'/Option2'} className={"nav2v"} style={(params)=>{return {color: params.isActive ? 'gold' : "white"}}}>Second counter</NavLink>
         </div>
             <Routes>
-                <Route path={'/'} element={<MainCounter/>}/>
-                <Route path={'/option2'} element={<MainCounterV2/>}/>
+                <Route path={'/Counter/*'} element={<MainCounter/>}/>
+                <Route path={'/Option2/*'} element={<MainCounterV2/>}/>
+                {/*<Route path={() => <MainCounter/>}/>*/}
             </Routes>
         </>
     );
